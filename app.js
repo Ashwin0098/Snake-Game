7a51ed804c637165105861beae517a9577c6345b
Ashwin <ashwinster98@gmail.com>
2024-10-26 19:08:48 +0800

    feat: Made some changes in CSS, added timer, gamesetup in JS

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
+let cells = Array.from(document.querySelectorAll('.grid div'));
 let score = 0;
 let speed = 200;
-let timer = 30;
-let gameLoop;
+let timer = 60;
+let timeInterval;
+let gameLoop, foodIndex;
 let snake = [2, 1, 0];
 let direction = 1;
+let gameActive = true;
 
 const boardSize = 20;
 const totalCells = boardSize * boardSize;
+const grid = document.querySelector('.grid');  
 const gameBoard = document.getElementById('gameBoard');
-let scoreDisplay = document.getElementById('scoreboard');
+const scoreDisplay = document.getElementById('scoreboard');
 const messageDisplay = document.getElementById('message'); 
 const timerDisplay = document.getElementById('timer');
 const resetButton = document.getElementById('Reset');
-const grid = document.querySelector('.grid');  
 
 for (let i =0; i<totalCells; i++) {
     const cell = document.createElement('div');
     gameBoard.appendChild(cell);
+}
+cells = Array.from(document.querySelectorAll('.grid div'));
+
+function startGame() {
+    timeInterval = setInterval(() => {
+    }, 1000);
+}
+function setupGame() {
+    placeFood();
+    timerDisplay.innerText = `Time: ${timer}`;
+    scoreDisplay.innerText = `Score: ${score}`;
+    cells[snake[0]].classList.add('snake');
+    document.addEventListener('keydown', changeDirection);
+    gameLoop = setInterval(moveSnake, speed);
+    countdown();
 }
